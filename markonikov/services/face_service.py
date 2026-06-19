c36e7c56b4d231440ad213fadfa91a452d837222
Prayuktha Lucky Reddy <mc240041025@iiti.ac.in>
2026-06-19 22:59:40 +0530

    Update face_service.py

--- markonikov/services/face_service.py
+++ markonikov/services/face_service.py
@@ -1,26 +1,26 @@
 import numpy as np
 from insightface.app import FaceAnalysis
 
-app = FaceAnalysis()
-app.prepare(ctx_id=0)
+_app = None
+
+def get_app():
+    global _app
+    if _app is None:
+        _app = FaceAnalysis()
+        _app.prepare(ctx_id=0)
+    return _app
 
 def detect_faces(image):
-    return app.get(image)
+    return get_app().get(image)
 
 def get_embedding(image):
-    faces = app.get(image)
-
+    faces = get_app().get(image)
     if len(faces) == 0:
         return None
-
     return faces[0].embedding
 
-
-
 def compare_embeddings(emb1, emb2):
-
     similarity = np.dot(emb1, emb2) / (
         np.linalg.norm(emb1) * np.linalg.norm(emb2)
     )
-
     return float(similarity)
